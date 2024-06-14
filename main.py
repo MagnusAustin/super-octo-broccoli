1c041795883973701d059e8c888a9445d7d11fec
MagnusAustin <manastare27@gmail.com>
2024-06-14 23:53:51 +0530

    Testing button

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route("/")
+def index():
     return render_template('index.html')
+
+@app.route("/signinpage")
+def signinpage():
+    return render_template('signinpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
