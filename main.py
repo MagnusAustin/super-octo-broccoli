690e4f1d72ac10b828f1c9fdde6dc2dc28a060d1
Swamesh B <swameshbam@gmail.com>
2024-06-15 00:13:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route('/')
+def home():
     return render_template('index.html')
 
-@app.route("/signinpage")
-def signinpage():
+@app.route("/signup")
+def signup():
     return render_template('signinpage.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
